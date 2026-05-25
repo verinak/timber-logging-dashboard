bdf6dead4e481362b43bd2dd68e99846fcd466eb
verinak <verinamichelk@gmail.com>
2026-05-25 23:35:30 +0300

    feat: create placeholder landing page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LandingComponent } from './pages/landing/landing.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LandingComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'timber-logging-dashboard';
---
+++ src/app/pages/landing/landing.component.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-landing',
+    imports: [CommonModule],
+    templateUrl: './landing.component.html',
+    styleUrl: './landing.component.css',
+})
+export class LandingComponent {}
