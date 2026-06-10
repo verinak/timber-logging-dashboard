8eb8be454619c2a15f1cfb3fa3f27a86164b1fee
verinak <verinamichelk@gmail.com>
2026-06-10 04:56:31 +0300

    feat: add login and signup screens

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,29 @@
 import { Routes } from '@angular/router';
 import { LandingComponent } from './pages/landing/landing.component';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: LandingComponent,
         title: 'Timber Console - All your logs in one place',
+    },
+    {
+        path: '',
+        component: AuthLayoutComponent,
+        children: [
+            {
+                path: 'login',
+                component: LoginComponent,
+                title: 'Log In | Timber',
+            },
+            {
+                path: 'signup',
+                component: RegisterComponent,
+                title: 'Sign Up | Timber',
+            },
+        ],
     },
 ];
