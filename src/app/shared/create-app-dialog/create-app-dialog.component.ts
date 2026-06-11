e9253701dd1549708b928796e18a8f4749d7b578
verinak <verinamichelk@gmail.com>
2026-06-11 14:56:30 +0300

    feat: implement app creation functionality

--- src/app/shared/create-app-dialog/create-app-dialog.component.ts
+++ src/app/shared/create-app-dialog/create-app-dialog.component.ts
@@ -1,20 +1,58 @@
 import {
     Component,
     EventEmitter,
+    inject,
     OnDestroy,
     OnInit,
     Output,
 } from '@angular/core';
 import { ShowModalService } from '../../services/show-modal/show-modal.service';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+    catchError,
+    debounceTime,
+    distinctUntilChanged,
+    filter,
+    of,
+    startWith,
+    switchMap,
+} from 'rxjs';
+import { AppsService } from '../../services/apps/apps.service';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-create-app-dialog',
-    imports: [],
+    imports: [ReactiveFormsModule],
     templateUrl: './create-app-dialog.component.html',
     styleUrl: './create-app-dialog.component.css',
 })
 export class CreateAppDialogComponent implements OnInit, OnDestroy {
+    private appsService = inject(AppsService);
+    private router = inject(Router);
+
+    appName = new FormControl('', {
+        nonNullable: true,
+        validators: [Validators.required, Validators.pattern(/^[a-z0-9-]+$/i)],
+    });
+
     constructor(private showModalService: ShowModalService) {}
+
+    // debouce name availability check
+    isNameAvailable = toSignal(
+        this.appName.valueChanges.pipe(
+            startWith(this.appName.value),
+            debounceTime(500),
+            distinctUntilChanged(),
+            filter((name) => name.trim().length > 0),
+            switchMap((name) =>
+                this.appsService
+                    .checkNameAvailability(name)
+                    .pipe(catchError(() => of(false))),
+            ),
+        ),
+        { initialValue: true },
+    );
 
     closeModal() {
         this.showModalService.close();
@@ -26,5 +64,25 @@ export class CreateAppDialogComponent implements OnInit, OnDestroy {
 
     ngOnDestroy() {
         document.body.classList.remove('modal-open');
+    }
+
+    createApplication() {
+        console.log('in submit');
+        if (this.appName.invalid) {
+            return;
+        }
+
+        // console.log(this.appName.value);
+
+        this.appsService.createApplication(this.appName.value).subscribe({
+            next: (app) => {
+                this.showModalService.close();
+                this.router.navigate(['/dashboard']); // i am tired ok don't judge
+            },
+            error: (err) => {
+                // handle error
+                console.error("couldn't create application", err);
+            },
+        });
     }
 }
